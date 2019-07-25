d1d65c34f11292ad9702a443ddc79878609a2162
Yingjie Zhu <1564319846@qq.com>
2019-07-25 09:25:30 +0800

    updates

--- wisdom-core/src/main/java/org/wisdom/pool/PoolTask.java
+++ wisdom-core/src/main/java/org/wisdom/pool/PoolTask.java
@@ -45,134 +45,134 @@ public class PoolTask {
     @Autowired
     ConsensusClient client;
 
-    @Scheduled(fixedDelay= 8*1000)
-    public void AdoptTopendingTask(){
-        List<TransPool> list=adoptTransPool.getAll();
-        long nowheight=wisdomBlockChain.currentHeader().nHeight;
-        Map<String,String> maps=new HashMap<>();
-        List<TransPool> newlist=new ArrayList<>();
-        int index=1;
-        for(TransPool t:list){
-            Transaction tran=t.getTransaction();
-            byte[] frompubhash=RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from));
-            long nownonce=accountDB.getNonce(frompubhash);
-            long nonce=tran.nonce;
+    @Scheduled(fixedDelay = 8 * 1000)
+    public void AdoptTopendingTask() {
+        List<TransPool> list = adoptTransPool.getAll();
+        long nowheight = wisdomBlockChain.currentHeader().nHeight;
+        Map<String, String> maps = new HashMap<>();
+        List<TransPool> newlist = new ArrayList<>();
+        int index = 1;
+        for (TransPool t : list) {
+            Transaction tran = t.getTransaction();
+            byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from));
+            long nownonce = accountDB.getNonce(frompubhash);
+            long nonce = tran.nonce;
             nownonce++;
-            if(nonce==nownonce){
-                if(TransactionCheck.checkoutPool(tran,wisdomBlockChain,configuration,accountDB,incubatorDB,rateTable,nowheight)){
-                    if(index>5000){
+            if (nonce == nownonce) {
+                if (TransactionCheck.checkoutPool(tran, wisdomBlockChain, configuration, accountDB, incubatorDB, rateTable, nowheight)) {
+                    if (index > 5000) {
                         break;
-                    }else{
-                        if(peningTransPool.hasExist(adoptTransPool.getKeyTrans(tran))){
-                            maps.put(Hex.encodeHexString(RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from))),adoptTransPool.getKey(t));
+                    } else {
+                        if (peningTransPool.hasExist(adoptTransPool.getKeyTrans(tran))) {
+                            maps.put(Hex.encodeHexString(RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from))), adoptTransPool.getKey(t));
                             newlist.add(t);
                             index++;
                         }
                     }
-                }else{
-                    maps.put(Hex.encodeHexString(RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from))),adoptTransPool.getKey(t));
+                } else {
+                    maps.put(Hex.encodeHexString(RipemdUtility.ripemd160(SHA3Utility.keccak256(tran.from))), adoptTransPool.getKey(t));
                 }
             }
         }
-        if(maps.size()>0){
+        if (maps.size() > 0) {
             adoptTransPool.remove(maps);
         }
-        if(newlist.size()>0){
+        if (newlist.size() > 0) {
             peningTransPool.add(newlist);
         }
     }
 
-    @Scheduled(fixedDelay= 2*60*1000)
-    public void clearPool(){
+    @Scheduled(fixedDelay = 2 * 60 * 1000)
+    public void clearPool() {
         //adoptTransPool
-        List<TransPool> list=adoptTransPool.getAll();
-        Map<String,String> maps=new HashMap<>();
-        for(TransPool transPool:list){
-            Transaction t=transPool.getTransaction();
-            long nonce=t.nonce;
-            byte[] from=t.from;
-            byte[] frompubhash=RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
+        List<TransPool> list = adoptTransPool.getAll();
+        Map<String, String> maps = new HashMap<>();
+        for (TransPool transPool : list) {
+            Transaction t = transPool.getTransaction();
+            long nonce = t.nonce;
+            byte[] from = t.from;
+            byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
             //nonce
-            long nownonce=accountDB.getNonce(frompubhash);
-            if(nownonce>=nonce){
-                maps.put(Hex.encodeHexString(t.from),adoptTransPool.getKeyTrans(t));
+            long nownonce = accountDB.getNonce(frompubhash);
+            if (nownonce >= nonce) {
+                maps.put(Hex.encodeHexString(t.from), adoptTransPool.getKeyTrans(t));
                 continue;
             }
-            long daysBetween=(new Date().getTime()-transPool.getDatetime()+1000000)/(60*60*24*1000);
-            if(daysBetween>=configuration.getPoolcleardays()){
-                maps.put(Hex.encodeHexString(frompubhash),adoptTransPool.getKeyTrans(t));
+            long daysBetween = (new Date().getTime() - transPool.getDatetime() + 1000000) / (60 * 60 * 24 * 1000);
+            if (daysBetween >= configuration.getPoolcleardays()) {
+                maps.put(Hex.encodeHexString(frompubhash), adoptTransPool.getKeyTrans(t));
                 continue;
             }
             //db
-            Transaction transaction=wisdomBlockChain.getTransaction(t.getHash());
-            if(transaction!=null){
-                maps.put(Hex.encodeHexString(frompubhash),adoptTransPool.getKeyTrans(t));
+            Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
+            if (transaction != null) {
+                maps.put(Hex.encodeHexString(frompubhash), adoptTransPool.getKeyTrans(t));
             }
         }
-        if(maps.size()>0){
+        if (maps.size() > 0) {
             adoptTransPool.remove(maps);
         }
         //peningTransPool
-        List<TransPool> pendinglist=peningTransPool.getAll();
-        List<String> pendinglists=new ArrayList<>();
-        List<Transaction> updatelist=new ArrayList<>();
-        for(TransPool transPool:pendinglist){
-            Transaction t=transPool.getTransaction();
-            long nonce=t.nonce;
-            byte[] from=t.from;
-            byte[] frompubhash=RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
+        List<TransPool> pendinglist = peningTransPool.getAll();
+        List<String> pendinglists = new ArrayList<>();
+        List<Transaction> updatelist = new ArrayList<>();
+        for (TransPool transPool : pendinglist) {
+            Transaction t = transPool.getTransaction();
+            long nonce = t.nonce;
+            byte[] from = t.from;
+            byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
             //nonce
-            long nownonce=accountDB.getNonce(frompubhash);
-            if(nownonce>=nonce){
+            long nownonce = accountDB.getNonce(frompubhash);
+            if (nownonce >= nonce) {
                 pendinglists.add(peningTransPool.getKeyTrans(t));
                 continue;
             }
-            long daysBetween=(new Date().getTime()-transPool.getDatetime()+1000000)/(60*60*24*1000);
-            if(daysBetween>=configuration.getPoolcleardays()){
+            long daysBetween = (new Date().getTime() - transPool.getDatetime() + 1000000) / (60 * 60 * 24 * 1000);
+            if (daysBetween >= configuration.getPoolcleardays()) {
                 pendinglists.add(peningTransPool.getKeyTrans(t));
                 continue;
             }
             //db
-            Transaction transaction=wisdomBlockChain.getTransaction(t.getHash());
-            if(transaction!=null){
+            Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
+            if (transaction != null) {
                 pendinglists.add(peningTransPool.getKeyTrans(t));
                 continue;
             }
             //高度
-            if(transPool.getState()==1){
-                long height=transPool.getHeight();
-                long nowheight=wisdomBlockChain.getCurrentTotalWeight();
-                if(nowheight>=height){
-                    Block b=wisdomBlockChain.getCanonicalBlock(height);
-                    if(b!=null){
-                        boolean state=true;
-                        for(Transaction tx:b.body){
-                            if(Arrays.equals(tx.getHash(),t.getHash())){
-                                state=false;
+            if (transPool.getState() == 1) {
+                long height = transPool.getHeight();
+                long nowheight = wisdomBlockChain.getCurrentTotalWeight();
+                if (nowheight >= height) {
+                    Block b = wisdomBlockChain.getCanonicalBlock(height);
+                    if (b != null) {
+                        boolean state = true;
+                        for (Transaction tx : b.body) {
+                            if (Arrays.equals(tx.getHash(), t.getHash())) {
+                                state = false;
                                 break;
                             }
                         }
-                        if(state==true){
+                        if (state == true) {
                             updatelist.add(t);
                         }
                     }
                 }
             }
         }
-        if(updatelist.size()>0){
-            peningTransPool.updatePool(updatelist,0,0);
+        if (updatelist.size() > 0) {
+            peningTransPool.updatePool(updatelist, 0, 0);
         }
-        if(pendinglists.size()>0){
+        if (pendinglists.size() > 0) {
             peningTransPool.remove(pendinglists);
         }
     }
 
-    @Scheduled(fixedDelay= 30*1000)
-    public void sendTranPool(){
-        List<TransPool> queuedlist=adoptTransPool.getAllFull();
-        List<Transaction> pengdinglist=peningTransPool.compare();
-        List<Transaction> totallist=new ArrayList<>();
-        for(TransPool transPool:queuedlist){
+    @Scheduled(fixedDelay = 30 * 1000)
+    public void sendTranPool() {
+        List<TransPool> queuedlist = adoptTransPool.getAllFull();
+        List<Transaction> pengdinglist = peningTransPool.compare();
+        List<Transaction> totallist = new ArrayList<>();
+        for (TransPool transPool : queuedlist) {
             totallist.add(transPool.getTransaction());
         }
         totallist.addAll(pengdinglist);
