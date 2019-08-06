4726bde1848650320a4921fd5642c04e4fd10872
YunMin <347594248@qq.com>
2019-08-06 16:52:09 +0800

    RPC

--- wisdom-core/src/main/java/org/wisdom/pool/ScheduleConfig.java
+++ wisdom-core/src/main/java/org/wisdom/pool/ScheduleConfig.java
@@ -1,19 +1,66 @@
 package org.wisdom.pool;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-/*
 @Configuration
-public class ScheduleConfig implements SchedulingConfigurer {
+@EnableAsync
+public class ScheduleConfig implements SchedulingConfigurer, AsyncConfigurer {
 
+    /**
+     * 并行任务
+     * @param scheduledTaskRegistrar
+     */
     @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        //设定一个长度10的定时任务线程池
-        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(100));
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        TaskScheduler taskScheduler = taskScheduler();
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler);
+    }
+
+    /**
+     * 多线程配置
+     * @return
+     */
+    @Bean(destroyMethod = "shutdown")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(20);
+        // 设置线程名前缀
+        scheduler.setThreadNamePrefix("task-");
+        // 线程内容执行完后60秒停在
+        scheduler.setAwaitTerminationSeconds(60);
+        // 等待所有线程执行完
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+
+    /**
+     * 异步任务
+     * @return
+     */
+    @Override
+    public Executor getAsyncExecutor() {
+        Executor executor = taskScheduler();
+        return executor;
+    }
+
+    /**
+     * 异常处理
+     * @return
+     */
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
     }
 }
-*/
