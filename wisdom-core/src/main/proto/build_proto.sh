22965780b9e8007557223c723d9ab93f6f36e3f9
Salpadding <sal@SalpaddingdeMacBook-Pro.local>
2019-08-06 14:59:46 +0800

    Update build_proto.sh

--- wisdom-core/src/main/proto/build_proto.sh
+++ wisdom-core/src/main/proto/build_proto.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-protoc --plugin=/usr/local/bin/protoc-gen-grpc-java \
+# download protoc-gen-grpc-java depends on your platform https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/1.22.1/
+protoc \
   --grpc-java_out="../java" wisdom.proto
 
 protoc --java_out="../java" wisdom.proto
