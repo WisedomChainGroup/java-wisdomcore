54409efadf25029f6d655fa689c610531d943bff
Salpadding <sal@SalpaddingdeMacBook-Pro.local>
2019-08-08 16:01:23 +0800

    Update TransactionHandler.java

--- wisdom-core/src/main/java/org/wisdom/sync/TransactionHandler.java
+++ wisdom-core/src/main/java/org/wisdom/sync/TransactionHandler.java
@@ -1,13 +1,16 @@
 package org.wisdom.sync;
 
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.wisdom.core.account.Transaction;
 import org.wisdom.p2p.Context;
 import org.wisdom.p2p.PeerServer;
 import org.wisdom.p2p.Plugin;
 import org.wisdom.p2p.WisdomOuterClass;
+import org.wisdom.service.CommandService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentMap;
@@ -18,21 +21,26 @@ public class TransactionHandler implements Plugin {
     private static final int CACHE_SIZE = 64;
     private ConcurrentMap<String, Boolean> transactionCache;
 
+    @Autowired
+    private CommandService commandService;
+
     public TransactionHandler() {
         this.transactionCache = new ConcurrentLinkedHashMap.Builder<String, Boolean>().maximumWeightedCapacity(CACHE_SIZE).build();
     }
 
     @Override
     public void onMessage(Context context, PeerServer server) {
-        if (context.getPayload().getCode() != WisdomOuterClass.Code.TRANSACTIONS){
+        if (context.getPayload().getCode() != WisdomOuterClass.Code.TRANSACTIONS) {
             return;
         }
         WisdomOuterClass.Transactions txs = (WisdomOuterClass.Transactions) context.getPayload().getBody();
         txs.getTransactionsList()
                 .stream()
-                .map(Utils::parseTransaction).forEach(y -> {
-                    
+                .map(Utils::parseTransaction).forEach(t -> {
+            byte[] traninfo = t.toRPCBytes();
+            commandService.verifyTransfer(traninfo);
         });
+        context.relay();
     }
 
     @Override
@@ -43,18 +51,9 @@ public class TransactionHandler implements Plugin {
     public void broadcastTransactions(List<Transaction> txs) {
         Optional.ofNullable(server)
                 .ifPresent(s -> {
-
+                    WisdomOuterClass.Transactions.Builder builder = WisdomOuterClass.Transactions.newBuilder();
+                    txs.stream().map(Utils::encodeTransaction).forEach(builder::addTransactions);
+                    s.broadcast(builder.build());
                 });
-    }
-
-    private void onTransaction(Context context, PeerServer server) {
-        WisdomOuterClass.Transaction tx = context.getPayload().getTransaction();
-        Transaction t = Utils.parseTransaction(tx);
-        if (transactionCache.containsKey(t.getHashHexString())) {
-            return;
-        }
-        transactionCache.put(t.getHashHexString(), true);
-        // TODO: 收到广播后的事务要进行处理
-        context.relay();
     }
 }
