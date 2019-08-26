eba83e3641be734477a782b2650ac58e6ba880e9
liuyong199311080810 <1991595613@qq.com>
2019-08-26 09:04:45 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
 
-env -S "$(tr '\n' ' ' < local.env)" ./gradlew run
+CUR=`dirname $0`
+
+ENV_FILE=${CUR=}/local.env
+env $(cat ${ENV_FILE} | grep -v '^#'| xargs) bash ./gradlew run
