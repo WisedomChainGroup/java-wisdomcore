d013779bd23708d7d5a045200b188693ea13cfa2
liuyong199311080810 <1991595613@qq.com>
2019-09-10 10:14:50 +0800

    add conflict code

--- wisdom-core/src/main/java/org/wisdom/pool/ClearPoolCronTask.java
+++ wisdom-core/src/main/java/org/wisdom/pool/ClearPoolCronTask.java
@@ -42,87 +42,93 @@ public class ClearPoolCronTask implements SchedulingConfigurer {
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        //adoptTransPool
-        List<TransPool> list = adoptTransPool.getAll();
-        IdentityHashMap<String, String> maps = new IdentityHashMap<>();
-        for (TransPool transPool : list) {
-            Transaction t = transPool.getTransaction();
-            long nonce = t.nonce;
-            byte[] from = t.from;
-            byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
-            //nonce
-            long nownonce = accountDB.getNonce(frompubhash);
-            if (nownonce >= nonce) {
-                maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
-                continue;
+        taskRegistrar.addTriggerTask(() -> {
+            //adoptTransPool
+            List<TransPool> list = adoptTransPool.getAll();
+            IdentityHashMap<String, String> maps = new IdentityHashMap<>();
+            for (TransPool transPool : list) {
+                Transaction t = transPool.getTransaction();
+                long nonce = t.nonce;
+                byte[] from = t.from;
+                byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
+                //nonce
+                long nownonce = accountDB.getNonce(frompubhash);
+                if (nownonce >= nonce) {
+                    maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
+                    continue;
+                }
+                long daysBetween = (new Date().getTime() - transPool.getDatetime()) / (60 * 60 * 1000);
+                if (daysBetween >= configuration.getPoolcleardays()) {
+                    maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
+                    continue;
+                }
+                //db
+                Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
+                if (transaction != null) {
+                    maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
+                }
             }
-            long daysBetween = (new Date().getTime() - transPool.getDatetime()) / (60 * 60 * 1000);
-            if (daysBetween >= configuration.getPoolcleardays()) {
-                maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
-                continue;
+            if (maps.size() > 0) {
+                adoptTransPool.remove(maps);
             }
-            //db
-            Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
-            if (transaction != null) {
-                maps.put(new String(Hex.encodeHexString(frompubhash)), adoptTransPool.getKeyTrans(t));
-            }
-        }
-        if (maps.size() > 0) {
-            adoptTransPool.remove(maps);
-        }
-        //peningTransPool
-        List<TransPool> pendinglist = peningTransPool.getAll();
-        List<Transaction> updatelist = new ArrayList<>();
-        IdentityHashMap<String, Long> map = new IdentityHashMap<>();
-        for (TransPool transPool : pendinglist) {
-            Transaction t = transPool.getTransaction();
-            long nonce = t.nonce;
-            byte[] from = t.from;
-            byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
-            //nonce
-            long nownonce = accountDB.getNonce(frompubhash);
-            if (nownonce >= nonce) {
-                map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
-                continue;
-            }
-            long daysBetween = (new Date().getTime() - transPool.getDatetime()) / (60 * 60 * 1000);
-            if (daysBetween >= configuration.getPoolcleardays()) {
-                map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
-                continue;
-            }
-            //db
-            Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
-            if (transaction != null) {
-                map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
-                continue;
-            }
-            //高度
-            if (transPool.getState() == 1) {
-                long height = transPool.getHeight();
-                long nowheight = wisdomBlockChain.getCurrentTotalWeight();
-                if (nowheight >= height) {
-                    Block b = wisdomBlockChain.getCanonicalBlock(height);
-                    if (b != null) {
-                        boolean state = true;
-                        for (Transaction tx : b.body) {
-                            if (Arrays.equals(tx.getHash(), t.getHash())) {
-                                state = false;
-                                break;
+            //peningTransPool
+            List<TransPool> pendinglist = peningTransPool.getAll();
+            List<Transaction> updatelist = new ArrayList<>();
+            IdentityHashMap<String, Long> map = new IdentityHashMap<>();
+            for (TransPool transPool : pendinglist) {
+                Transaction t = transPool.getTransaction();
+                long nonce = t.nonce;
+                byte[] from = t.from;
+                byte[] frompubhash = RipemdUtility.ripemd160(SHA3Utility.keccak256(from));
+                //nonce
+                long nownonce = accountDB.getNonce(frompubhash);
+                if (nownonce >= nonce) {
+                    map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
+                    continue;
+                }
+                long daysBetween = (new Date().getTime() - transPool.getDatetime()) / (60 * 60 * 1000);
+                if (daysBetween >= configuration.getPoolcleardays()) {
+                    map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
+                    continue;
+                }
+                //db
+                Transaction transaction = wisdomBlockChain.getTransaction(t.getHash());
+                if (transaction != null) {
+                    map.put(new String(Hex.encodeHexString(frompubhash)), t.nonce);
+                    continue;
+                }
+                //高度
+                if (transPool.getState() == 1) {
+                    long height = transPool.getHeight();
+                    long nowheight = wisdomBlockChain.getCurrentTotalWeight();
+                    if (nowheight >= height) {
+                        Block b = wisdomBlockChain.getCanonicalBlock(height);
+                        if (b != null) {
+                            boolean state = true;
+                            for (Transaction tx : b.body) {
+                                if (Arrays.equals(tx.getHash(), t.getHash())) {
+                                    state = false;
+                                    break;
+                                }
                             }
-                        }
-                        if (state) {
-                            updatelist.add(t);
+                            if (state) {
+                                updatelist.add(t);
+                            }
                         }
                     }
                 }
             }
-        }
-        if (updatelist.size() > 0) {
-            peningTransPool.updatePool(updatelist, 0, 0);
-        }
-        if (map.size() > 0) {
-            peningTransPool.remove(map);
-        }
+            if (updatelist.size() > 0) {
+                peningTransPool.updatePool(updatelist, 0, 0);
+            }
+            if (map.size() > 0) {
+                peningTransPool.remove(map);
+            }
+        }, triggerContext -> {
+            //任务触发，可修改任务的执行周期
+            CronTrigger trigger = new CronTrigger(ipcConfig.getClearCycle());
+            return trigger.nextExecutionTime(triggerContext);
+        });
     }
 
 }
