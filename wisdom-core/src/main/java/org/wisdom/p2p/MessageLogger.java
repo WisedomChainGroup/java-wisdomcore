bfc8424781037a206d6920b9b349d15453fdc954
zhuyingjie <m6567fc@outlook.com>
2019-10-17 16:05:58 +0800

    add jvm max stack size parameter

--- wisdom-core/src/main/java/org/wisdom/p2p/MessageLogger.java
+++ wisdom-core/src/main/java/org/wisdom/p2p/MessageLogger.java
@@ -2,15 +2,22 @@ package org.wisdom.p2p;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageLogger implements Plugin {
     private static final Logger logger = LoggerFactory.getLogger(MessageLogger.class);
 
+    @Value("${p2p.enable-message-log}")
+    private boolean enableMessageLog;
+
     @Override
     public void onMessage(Context context, PeerServer server) {
-//        logger.info("receive " + context.getPayload().getCode().name() + " message from remote peer " + context.getPayload().getRemote().toString() + " " + context.getPayload().getMessage().toString());
+        if (!enableMessageLog){
+            return;
+        }
+        logger.info("receive " + context.getPayload().getCode().name() + " message from remote peer " + context.getPayload().getRemote().hostPort());
     }
 
     @Override
