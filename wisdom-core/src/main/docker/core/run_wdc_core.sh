fe7d15da5398b5abad9f0daf92fd0efaea00ed71
YunMin <347594248@qq.com>
2019-10-21 17:52:15 +0800

    GC

--- wisdom-core/src/main/docker/core/run_wdc_core.sh
+++ wisdom-core/src/main/docker/core/run_wdc_core.sh
@@ -3,4 +3,4 @@ if [ "$WDC_MINER_COINBASE" = "" ]; then
 	echo "!!! ERROR !!! ENV WDC_MINER_COINBASE not set."
 	exit 1
 fi
-java -Xss8m -XX:MaxPermSize=128m -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -jar /app/app.jar
+java -Xss8m -Xms512m -Xmx4096m -XX:MaxNewSize=1024m -XX:MaxPermSize=1024m -jar /app/app.jar
