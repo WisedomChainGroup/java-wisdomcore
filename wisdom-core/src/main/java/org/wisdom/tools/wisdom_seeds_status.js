3bb5f82636dd72508fe2962b8dacca8327a70251
zhuyingjie <m6567fc@outlook.com>
2019-10-23 11:01:29 +0800

    refactor nodejs tool

--- wisdom-core/src/main/java/org/wisdom/tools/wisdom_seeds_status.js
+++ wisdom-core/src/main/java/org/wisdom/tools/wisdom_seeds_status.js
@@ -9,7 +9,8 @@
 
 const axios = require('axios')
 
-SEEDS = [
+const SEEDS = [
+
     '120.76.101.153',
     '47.74.183.249',
     '47.74.216.251',
@@ -18,17 +19,17 @@ SEEDS = [
     '47.56.67.236'
 ]
 
-const asyncs = []
 
-for(let h of SEEDS){
-    asyncs.push(
-        axios.get(`http://${h}:19585/block/-1`)
+
+Promise.all(
+    SEEDS.map(h =>
+        axios
+            .get(`http://${h}:19585/block/-1`)
             .then(resp => resp.data)
             .then(data => console.log(
                 `${h} \t height = ${data.nHeight} \t hash = ${data.blockHash} \t prev hash = ${data.hashPrevBlock}`
             ))
             .catch(e => console.error(`cannot connect to ${h}`))
+        )
     )
-}
-
-Promise.all(asyncs)
+    .then(() => {})
