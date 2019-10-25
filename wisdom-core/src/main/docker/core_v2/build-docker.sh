9d471f562c5f15a804ef4e7c847f5c4a73d734d2
zhuyingjie <m6567fc@outlook.com>
2019-10-25 14:47:53 +0800

    create shell scripts for docker builds

--- wisdom-core/src/main/docker/core_v2/build-docker.sh
+++ wisdom-core/src/main/docker/core_v2/build-docker.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 IMAGE=wisdomchain/wdc_core
+
+# directory of this file
 CUR=$(dirname $0)
+CUR=`cd $CUR; pwd`
+
+rm -rf $CUR/build
+mkdir $CUR/build
 
 # parse -t -tag arguments as $TAG
-source $CUR/../parse_tag.sh
+source $CUR/arg_parse.sh
 
+# project root path java-wisdomcore/wisdomcore
 PROJECT_ROOT=$CUR/../../../..
 PROJECT_ROOT=`cd $PROJECT_ROOT; pwd`
+
+
+cd $PROJECT_ROOT/..
 bash $PROJECT_ROOT/../gradlew jar
+
 cp $PROJECT_ROOT/build/libs/wisdom*.jar $CUR/build
 
 SUFFIX=:$TAG
--- wisdom-core/src/main/docker/server_jre8_with_dependencies/build-docker.sh
+++ wisdom-core/src/main/docker/server_jre8_with_dependencies/build-docker.sh
@@ -6,6 +6,10 @@ IMAGE=wisdomchain/server_jre8_with_dependencies
 
 # directory of this file
 CUR=$(dirname $0)
+CUR=`cd $CUR; pwd`
+
+rm -rf $CUR/build
+mkdir $CUR/build
 
 # parse -t -tag arguments as $TAG
 source $CUR/arg_parse.sh
@@ -14,17 +18,20 @@ source $CUR/arg_parse.sh
 PROJECT_ROOT=$CUR/../../../..
 PROJECT_ROOT=`cd $PROJECT_ROOT; pwd`
 
-bash $PROJECT_ROOT/../gradlew copyDependencies
+
+cd $PROJECT_ROOT/..
+bash $PROJECT_ROOT/../gradlew jar
 
 # copy dependencies to tempory directory
-cp $PROJECT_ROOT/build/libs/lib/*.jar $CUR/build/
-cp -r $PROJECT_ROOT/build/resources/wisdom-genesis-generator.json $CUR/build/
+cp $PROJECT_ROOT/build/libs/lib/*.jar $CUR/build
+cp $PROJECT_ROOT/src/main/resources/genesis/wisdom-genesis-generator.json $CUR/build
 SUFFIX=:$TAG
 if [[ $SUFFIX == ':' ]]; then
   SUFFIX=''
 fi
 
+cd $CUR
 docker build -f $CUR/Dockerfile -t $IMAGE$SUFFIX $CUR
 
 # clean tempory files
-rm -rf $CUR/build/*
+rm -rf $CUR/build
