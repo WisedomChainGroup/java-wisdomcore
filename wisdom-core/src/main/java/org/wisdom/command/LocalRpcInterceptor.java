ff8c04da7b24748f4d9521ccf7c8256438faf8ae
YunMin <347594248@qq.com>
2019-10-30 10:56:11 +0800

    JWT token

--- wisdom-core/src/main/java/org/wisdom/command/LocalRpcInterceptor.java
+++ wisdom-core/src/main/java/org/wisdom/command/LocalRpcInterceptor.java
@@ -20,32 +20,35 @@ package org.wisdom.command;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.wisdom.util.JWTUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class RpcInterceptor implements HandlerInterceptor {
+public class LocalRpcInterceptor implements HandlerInterceptor {
 
-    private static Logger logger = LoggerFactory.getLogger(RpcInterceptor.class);
+    private static Logger logger = LoggerFactory.getLogger(LocalRpcInterceptor.class);
 
+    @Value("${wisdom.localonly}")
+    private boolean localonly;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String token = request.getHeader("token");
-        //判断
-        if (token != null && token != "") {
-            if(token.equals("NUMtD0dEXungVX7eLuXkEurH5BCJzw")){
-                return true;
-            }else{
+        if(localonly){
+            String token = request.getHeader("token");
+            //判断
+            if (token != null && token != "") {
+                return JWTUtil.parseJWT(token);
+            } else {
+//            throw new RuntimeException("无token，调用失败");
+                logger.warn("The local connection has been started,Illegal connection request from " + request.getRemoteHost() + ":" + request.getRemotePort());
                 return false;
             }
-        } else {
-//            throw new RuntimeException("无token，调用失败");
-            logger.warn("Illegal incubate connection request from " + request.getRemoteHost() + ":" + request.getRemotePort());
-            return false;
         }
+        return true;
     }
 }
