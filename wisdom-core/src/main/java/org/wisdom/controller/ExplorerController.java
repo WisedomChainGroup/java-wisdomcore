259209184aeacb42e05d0442c0cf26517eadb054
YunMin <347594248@qq.com>
2019-11-06 09:43:17 +0800

    localonly

--- wisdom-core/src/main/java/org/wisdom/controller/ExplorerController.java
+++ wisdom-core/src/main/java/org/wisdom/controller/ExplorerController.java
@@ -1,9 +1,14 @@
 package org.wisdom.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExplorerController {
 
+    @GetMapping(value = "/WisdomCore/ExplorerInfo")
+    public Object getExplorerInfo(){
+        return null;
+    }
 
 }
