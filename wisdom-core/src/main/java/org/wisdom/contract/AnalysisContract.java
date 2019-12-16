ac6a221ff6e2e9962fa9962a74656e01db679cba
YunMin <347594248@qq.com>
2019-12-16 10:04:50 +0800

    update packageMiner code

--- wisdom-core/src/main/java/org/wisdom/contract/AnalysisContract.java
+++ wisdom-core/src/main/java/org/wisdom/contract/AnalysisContract.java
@@ -1,12 +1,11 @@
 package org.wisdom.contract;
 
-import org.wisdom.ApiResult.APIResult;
 import org.wisdom.db.AccountState;
 
 import java.util.List;
 
 public interface AnalysisContract extends RLPBeanInterface {
-    
+
     List<AccountState> update(List<AccountState> accountStateList);
 
 }
