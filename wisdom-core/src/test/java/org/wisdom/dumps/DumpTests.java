15e619067ed3d32fe890adc4b31d9245d57f8950
Sal <m6567fc@outlook.com>
2019-12-30 16:52:00 +0800

    Update DumpTests.java

--- wisdom-core/src/test/java/org/wisdom/dumps/DumpTests.java
+++ wisdom-core/src/test/java/org/wisdom/dumps/DumpTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.tdf.common.serialize.Codec;
+import org.tdf.common.store.ByteArrayMapStore;
+import org.tdf.common.trie.Trie;
+import org.tdf.common.trie.TrieImpl;
 import org.tdf.common.util.ByteArraySet;
 import org.tdf.common.util.HexBytes;
 import org.tdf.rlp.RLPCodec;
@@ -15,17 +19,17 @@ import org.wisdom.context.TestContext;
 import org.wisdom.core.Block;
 import org.wisdom.core.WisdomBlockChain;
 import org.wisdom.core.account.AccountDB;
-import org.wisdom.core.account.Transaction;
+import org.wisdom.crypto.HashUtil;
 import org.wisdom.db.AccountState;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
@@ -82,12 +86,12 @@ public class DumpTests {
     }
 
     @Test
-    public void restoreDumps(){
+    public void restoreDumps() {
 
     }
 
     @Test
-    public void createNewGenesis() throws Exception{
+    public void createNewGenesis() throws Exception {
         String blocksDirectory = "z:\\dumps\\blocks";
         String outputDirectory = "z:\\dumps\\accounts";
         int height = 800040;
@@ -107,24 +111,24 @@ public class DumpTests {
         Arrays.stream(files)
                 .sorted(Comparator.comparingInt(x -> Integer.parseInt(x.getName().split("\\.")[1])))
                 .flatMap(x -> {
-                    try{
+                    try {
                         byte[] bytes = Files.readAllBytes(x.toPath());
                         return Arrays.stream(RLPElement.fromEncoded(bytes).as(Block[].class));
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
                 })
                 .filter(b -> b.nHeight <= height)
                 .peek(b -> {
-                    if(b.nHeight == height) newGenesis[0] = b;
+                    if (b.nHeight == height) newGenesis[0] = b;
                 })
                 .flatMap(b -> b.body.stream())
                 .forEach(tx -> {
-                    if(!Arrays.equals(zeroPublicKey, tx.from)){
+                    if (!Arrays.equals(zeroPublicKey, tx.from)) {
                         assert tx.from.length == 32 || tx.from.length == 20;
-                        set.add(tx.from.length == 32 ? PublicKeyHash.fromPublicKey(tx.from).getPublicKeyHash(): tx.from);
+                        set.add(tx.from.length == 32 ? PublicKeyHash.fromPublicKey(tx.from).getPublicKeyHash() : tx.from);
                     }
-                    if(!Arrays.equals(zeroPublicKeyHash, tx.to)){
+                    if (!Arrays.equals(zeroPublicKeyHash, tx.to)) {
                         assert tx.to.length == 20;
                         set.add(tx.to);
                     }
@@ -133,9 +137,9 @@ public class DumpTests {
         assert newGenesis[0] != null;
         List<AccountState> states = set.stream()
                 .map(x -> {
-                    try{
+                    try {
                         return accountDB.getAccounstate(x, height);
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                         System.out.println(HexBytes.encode(x));
                         return null;
@@ -155,7 +159,7 @@ public class DumpTests {
     }
 
     @Test
-    public void parse() throws Exception{
+    public void parse() throws Exception {
         String genesisDirectory = "z:\\dumps\\accounts";
         File file = Paths.get(genesisDirectory).toFile();
         if (!file.isDirectory()) throw new RuntimeException(genesisDirectory + " is not a valid directory");
@@ -163,12 +167,20 @@ public class DumpTests {
         if (files == null || files.length == 0) throw new RuntimeException("empty directory " + file);
         File lastGenesis = Arrays.stream(files)
                 .filter(f -> f.getName().matches("genesis\\.[0-9]+\\.rlp"))
-                .sorted((x,y) -> (int) (Long.parseLong(y.getName().split("\\.")[1]) -  Long.parseLong(x.getName().split("\\.")[1])))
+                .sorted((x, y) -> (int) (Long.parseLong(y.getName().split("\\.")[1]) - Long.parseLong(x.getName().split("\\.")[1])))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("unreachable"));
         RLPElement el = RLPElement.fromEncoded(Files.readAllBytes(lastGenesis.toPath()));
         Block genesis = el.get(0).as(Block.class);
         AccountState[] genesisStates = el.get(1).as(AccountState[].class);
-
+        Trie<byte[], AccountState> trie = TrieImpl.newInstance(
+                HashUtil::keccak256, new ByteArrayMapStore<>(),
+                Codec.identity(),
+                Codec.newInstance(RLPCodec::encode, x -> RLPCodec.decode(x, AccountState.class))
+        );
+        Arrays.asList(genesisStates)
+                .forEach(s -> trie.put(s.getAccount().getPubkeyHash(), s));
+        byte[] root = trie.commit();
+        System.out.println(HexBytes.encode(root));
     }
 }
