815847ddb6b72e482654810c44cac1fe3eb6d17a
YunMin <347594248@qq.com>
2020-03-02 10:16:28 +0800

    update AccountStateUpdater.java

--- wisdom-core/src/main/java/org/wisdom/db/AccountStateUpdater.java
+++ wisdom-core/src/main/java/org/wisdom/db/AccountStateUpdater.java
@@ -229,6 +229,7 @@ public class AccountStateUpdater extends AbstractStateUpdater<AccountState> {
         if (balance < 0) {
             throw new RuntimeException("math overflow");
         }
+        account.setNonce(tx.nonce);
         account.setBalance(balance);
         account.setBlockHeight(height);
         accountState.setAccount(account);
