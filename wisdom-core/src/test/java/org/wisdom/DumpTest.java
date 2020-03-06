1f81c7fba1d00a2140b784029489420dbdda42f8
Sal <m6567fc@outlook.com>
2020-03-06 15:44:57 +0800

    Update DumpTest.java

--- wisdom-core/src/test/java/org/wisdom/DumpTest.java
+++ wisdom-core/src/test/java/org/wisdom/DumpTest.java
@@ -8,16 +8,20 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.tdf.common.store.DBSettings;
 import org.tdf.common.store.LevelDb;
+import org.tdf.common.util.FastByteComparisons;
+
+import java.util.Optional;
 
 @RunWith(JUnit4.class)
 public class DumpTest {
-    static final String parentHash = "33d0e70838ff832435f445cd2a0386f92ba6c1a03f1a5650f5fef3878b53c732";
-    static final String blockHash = "f101aa7868e9d243f1f4bfec659f240fbe785a56528656349883febf4061e0c1";
-    static final String directory = "C:\\Users\\Sal\\Desktop\\dumped";
+    static final String blockHash = "8f6e69a4b2aba4c61622f82a53539796dfbf62f79eb49278fb89d03b6fba1592";
+    static final String directory = "C:\\Users\\Sal\\Desktop\\roots";
     static final String[] names = new String[]{
-            "validator-trie-roots",
-            "account-trie-roots",
-            "asset-code-trie-roots"
+            "account-trie-roots-fault",
+            "account-trie-roots-true",
+            "account-trie-roots(2)",
+            "account-trie-roots(3)",
+            "account-trie-roots(4)"
     };
 
     @Test
@@ -26,11 +30,34 @@ public class DumpTest {
             LevelDb db1 =
                     new LevelDb(Iq80DBFactory.factory, directory, name);
             db1.init(DBSettings.DEFAULT);
-            assert db1.containsKey(Hex.decodeHex(parentHash.toCharArray()));
-            if(!db1.containsKey(Hex.decodeHex(blockHash.toCharArray()))){
+            if(db1.containsKey(Hex.decodeHex(blockHash.toCharArray()))){
                 System.out.println(name);
+                System.out.println(Hex.encodeHexString(db1.get(Hex.decodeHex(blockHash)).get()));
             }
         }
 
+    }
+
+    @Test
+    public void test2() throws Exception{
+
+            LevelDb db1 =
+                    new LevelDb(Iq80DBFactory.factory, directory, names[0]);
+            db1.init(DBSettings.DEFAULT);
+
+
+        LevelDb db2 =
+                new LevelDb(Iq80DBFactory.factory, directory, names[1]);
+        db2.init(DBSettings.DEFAULT);
+
+        db1.forEach((k, v) -> {
+            Optional<byte[]> bytes = db2.get(k);
+            if(!bytes.isPresent()){
+                System.out.println(Hex.encodeHex(k));
+            }
+            if(bytes.isPresent() && !FastByteComparisons.equal(bytes.get(), v)){
+                System.out.println(Hex.encodeHex(k));
+            }
+        });
     }
 }
