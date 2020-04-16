ef861d97ffaa85919b2328284cb403dede9a1664
YunMin <347594248@qq.com>
2020-04-16 14:19:01 +0800

    Deprecated DllController.java

--- wisdom-core/src/main/java/org/wisdom/controller/DllController.java
+++ wisdom-core/src/main/java/org/wisdom/controller/DllController.java
@@ -10,7 +10,8 @@ import org.wisdom.command.Dllparameter;
 import org.wisdom.encoding.JSONEncodeDecoder;
 import org.wisdom.service.DllService;
 
-@RestController
+//@RestController
+@Deprecated
 public class DllController {
 
     @Autowired
@@ -19,15 +20,15 @@ public class DllController {
     @Autowired
     DllService dllService;
 
-    @RequestMapping(value="/dll",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
-    public Object ddl(@RequestBody byte[] jsonParam){
-        Dllparameter dllparameter= jsonEncodeDecoder.decode(jsonParam, Dllparameter.class);
-        if(dllparameter==null || (dllparameter.getJarname()==null || dllparameter.getJarname().equals("")) ||
-                (dllparameter.getClasspackage()==null || dllparameter.getJarname().equals("")) ||
-                (dllparameter.getMethodname()==null || dllparameter.equals(""))){
-            return APIResult.newFailResult(5000,"Parameter conversion error");
-        }
-        return dllService.CallMethod(dllparameter);
-    }
+//    @RequestMapping(value="/dll",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+//    public Object ddl(@RequestBody byte[] jsonParam){
+//        Dllparameter dllparameter= jsonEncodeDecoder.decode(jsonParam, Dllparameter.class);
+//        if(dllparameter==null || (dllparameter.getJarname()==null || dllparameter.getJarname().equals("")) ||
+//                (dllparameter.getClasspackage()==null || dllparameter.getJarname().equals("")) ||
+//                (dllparameter.getMethodname()==null || dllparameter.equals(""))){
+//            return APIResult.newFailResult(5000,"Parameter conversion error");
+//        }
+//        return dllService.CallMethod(dllparameter);
+//    }
 
 }
