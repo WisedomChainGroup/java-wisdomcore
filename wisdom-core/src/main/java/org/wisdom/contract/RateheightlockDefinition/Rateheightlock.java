9e078ba094c8b6fac66ab58f3308ba0d021d4dcc
Zt <840043122@qq.com>
2020-04-26 16:13:40 +0800

    add Rateheightlock

---
+++ wisdom-core/src/main/java/org/wisdom/contract/RateheightlockDefinition/Rateheightlock.java
@@ -0,0 +1,62 @@
+package org.wisdom.contract.RateheightlockDefinition;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.tdf.common.util.ByteArrayMap;
+import org.tdf.rlp.RLP;
+import org.tdf.rlp.RLPCodec;
+import org.tdf.rlp.RLPDecoding;
+import org.tdf.rlp.RLPElement;
+import org.wisdom.contract.AnalysisContract;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rateheightlock implements AnalysisContract {
+
+    @RLP(0)
+    private byte[] assetHash;
+    @RLP(1)
+    private long onetimedepositmultiple;
+    @RLP(2)
+    private int withdrawperiodheight;
+    @RLP(3)
+    private BigDecimal withdrawrate;
+    @RLP(4)
+    private byte[] dest;
+    @RLP(5)
+    @RLPDecoding(as = ByteArrayMap.class)
+    private Map<byte[], Long> statelist;
+
+    @Override
+    public byte[] RLPserialization() {
+        return RLPCodec.encode(Rateheightlock.builder()
+                .assetHash(this.assetHash)
+                .onetimedepositmultiple(this.onetimedepositmultiple)
+                .withdrawperiodheight(this.withdrawperiodheight)
+                .withdrawrate(this.withdrawrate)
+                .dest(this.dest)
+                .statelist(this.statelist).build());
+    }
+
+    @Override
+    public boolean RLPdeserialization(byte[] payload) {
+        Rateheightlock rateheightlock = RLPElement.fromEncoded(payload).as(Rateheightlock.class);
+        if (rateheightlock == null) {
+            return false;
+        }
+        this.assetHash = rateheightlock.getAssetHash();
+        this.onetimedepositmultiple = rateheightlock.getOnetimedepositmultiple();
+        this.withdrawperiodheight = rateheightlock.getWithdrawperiodheight();
+        this.withdrawrate = rateheightlock.getWithdrawrate();
+        this.dest = rateheightlock.getDest();
+        this.statelist = rateheightlock.getStatelist();
+        return true;
+    }
+}
---
+++ wisdom-core/src/main/java/org/wisdom/contract/RateheightlockDefinition/RateheightlockDeposit.java
@@ -0,0 +1,35 @@
+package org.wisdom.contract.RateheightlockDefinition;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.tdf.rlp.RLP;
+import org.tdf.rlp.RLPCodec;
+import org.tdf.rlp.RLPElement;
+import org.wisdom.contract.AnalysisContract;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RateheightlockDeposit implements AnalysisContract {
+    @RLP(0)
+    private long value;
+
+    @Override
+    public byte[] RLPserialization() {
+        return RLPCodec.encode(RateheightlockDeposit.builder()
+                .value(this.value).build());
+    }
+
+    @Override
+    public boolean RLPdeserialization(byte[] payload) {
+        RateheightlockDeposit rateheightlockDeposit = RLPElement.fromEncoded(payload).as(RateheightlockDeposit.class);
+        if (rateheightlockDeposit == null) {
+            return false;
+        }
+        this.value = rateheightlockDeposit.getValue();
+        return true;
+    }
+}
