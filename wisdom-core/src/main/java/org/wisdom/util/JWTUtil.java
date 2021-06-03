6f4551acdb55013c39e73bcf0c3f7594ea8e16d8
zhuyingjie <m6567fc@outlook.com>
2021-06-03 17:38:01 +0800

    Update JWTUtil.java

--- wisdom-core/src/main/java/org/wisdom/util/JWTUtil.java
+++ wisdom-core/src/main/java/org/wisdom/util/JWTUtil.java
@@ -1,14 +1,51 @@
 package org.wisdom.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+/**
+ *
+ //The JWT signature algorithm we will be using to sign the token
+ SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
-import java.util.Date;
+ long nowMillis = System.currentTimeMillis();
+ Date now = new Date(nowMillis);
+
+ //We will sign our JWT with our ApiKey secret
+ byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(APP_ID + APP_SECRET);
+ Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+ //Let's set the JWT Claims
+ JwtBuilder builder = Jwts.builder().setId(id)
+ .setIssuedAt(now)
+ .setSubject(subject)
+ .setIssuer(issuer)
+ .signWith(signingKey, signatureAlgorithm);
+
+ //if it has been specified, let's add the expiration
+ if (ttlMillis >= 0) {
+ long expMillis = nowMillis + ttlMillis;
+ Date exp = new Date(expMillis);
+ builder.setExpiration(exp);
+ }
+
+ //Builds the JWT and serializes it to a compact, URL-safe string
+ return builder.compact();
+
+ */
+
+/**
+ *         try {
+ *             //This line will throw an exception if it is not a signed JWS (as expected)
+ *             Claims claims = Jwts.parser()
+ *                     .setSigningKey(DatatypeConverter.parseBase64Binary(APP_ID + APP_SECRET))
+ *                     .parseClaimsJws(jwt).getBody();
+ * //            System.out.println("ID: " + claims.getId());
+ * //            System.out.println("Subject: " + claims.getSubject());
+ * //            System.out.println("Issuer: " + claims.getIssuer());
+ * //            System.out.println("Expiration: " + claims.getExpiration());
+ *         }catch (Exception e){
+ *             return false;
+ *         }
+ *         return true;
+ */
 
 public class JWTUtil {
     private final static String APP_ID = "YHWS845682HYESE12yhsd187451289";
@@ -19,50 +56,11 @@ public class JWTUtil {
 
     //Sample method to construct a JWT
     public static String createJWT(long ttlMillis) {
-
-        //The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(APP_ID + APP_SECRET);
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(id)
-                .setIssuedAt(now)
-                .setSubject(subject)
-                .setIssuer(issuer)
-                .signWith(signingKey, signatureAlgorithm);
-
-        //if it has been specified, let's add the expiration
-        if (ttlMillis >= 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            builder.setExpiration(exp);
-        }
-
-        //Builds the JWT and serializes it to a compact, URL-safe string
-        return builder.compact();
-
+        return "";
     }
 
     //Sample method to validate and read the JWT
     public static boolean parseJWT(String jwt) {
-        try {
-            //This line will throw an exception if it is not a signed JWS (as expected)
-            Claims claims = Jwts.parser()
-                    .setSigningKey(DatatypeConverter.parseBase64Binary(APP_ID + APP_SECRET))
-                    .parseClaimsJws(jwt).getBody();
-//            System.out.println("ID: " + claims.getId());
-//            System.out.println("Subject: " + claims.getSubject());
-//            System.out.println("Issuer: " + claims.getIssuer());
-//            System.out.println("Expiration: " + claims.getExpiration());
-        }catch (Exception e){
-            return false;
-        }
         return true;
     }
 
